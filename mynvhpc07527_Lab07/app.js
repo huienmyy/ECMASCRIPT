8ada7239754dd7cdd022b455abb29515dbbd185c
huienmyy <mynvhpc07527@fpt.edu.vn>
2024-02-20 21:11:56 +0700

    ESLint

---
+++ mynvhpc07527_Lab07/app.js
@@ -0,0 +1,6 @@
+const string2 = 'Hello World';
+const string3 = 'Hello World';
+const string4 = 'Hello World';
+
+const stringFinal = `${string3 + string4}198719823${string2}`;
+console.log(stringFinal); 
